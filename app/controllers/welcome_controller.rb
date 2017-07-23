a98edf7c4cf6b8e08376352e08e226adee6d7223
Liu Yuan Xin <Yuanxin1007@gmail.com>
2017-07-23 16:31:28 +0800

    create group index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+  def index
+    flash[:notice] = "早安！你好！"
+    flash[:alert] = "Sorry, you're late!"
+    flash[:warning] = "PLease take attention for the time"
+  end
+
+
+
 end
