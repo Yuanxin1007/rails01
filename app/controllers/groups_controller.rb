0faa7883806bd882c099fc27572d586d556f0d5e
Liu Yuan Xin <Yuanxin1007@gmail.com>
2017-07-23 22:27:47 +0800

    implement group#create#eidt

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -7,9 +7,25 @@ class GroupsController < ApplicationController
    @group = Group.new
   end
 
+def edit
+  @group = Group.find(params[:id])
+end
+
+def create
+  @group = Group.new(group_pramas)
+  @group.save
+  redirect_to groups_path
+end
+
   def show
     @group = Group.find(params[:id])
   end
 
+private
+
+def group_pramas
+  params.require(:group).permit(:title, :description)
+
+end
 
 end
