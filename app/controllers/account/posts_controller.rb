4807bbba05b29df1edcb291ba2fb2a4f452c1eb6
Liu Yuan Xin <Yuanxin1007@gmail.com>
2017-07-31 22:03:37 +0800

    implement my posts

---
+++ app/controllers/account/posts_controller.rb
@@ -0,0 +1,8 @@
+class Account::PostsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+    @posts = current_user.posts.paginate(:page => params[:page], :per_page => 10)
+  end
+
+end
